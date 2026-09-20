4e23aba52d1b941f1c2ca0c8d6b20962c59f2163
NITISH KUMAR <amyhod3@gmail.com>
2026-09-20 22:06:42 +0530

    chore: refresh wide Flipkart onboarding assets

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ordergrid-product-v29';
+const CACHE='ordergrid-product-v30';
 const ASSETS=[
   './','./index.html','./styles.css','./finance.css','./premium.css','./overview-premium.css','./dashboard.css',
   './customer.css','./control-center.css','./automation-center.css','./fulfilment.css',
