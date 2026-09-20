b926fbd56e91cb83764368499f32754e2c37aa9c
NITISH KUMAR <amyhod3@gmail.com>
2026-09-20 14:28:52 +0530

    Refresh browser cache for commercial Autopilot controls

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE='ordergrid-product-v14';
+const CACHE='ordergrid-product-v15';
 const ASSETS=[
   './','./index.html','./styles.css','./finance.css','./premium.css','./dashboard.css',
   './customer.css','./control-center.css','./automation-center.css','./fulfilment.css',
