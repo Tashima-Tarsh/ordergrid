f9054d82eb765fc1fdd6a48a963eb198fc0cfb86
NITISH KUMAR <amyhod3@gmail.com>
2026-09-21 10:10:14 +0530

    Add managed retailer OTP worker command

---
+++ src/migrations/024_managed_retailer_otp.sql
@@ -0,0 +1,7 @@
+-- Managed browser execution can accept a user-supplied retailer OTP without exposing a local browser.
+alter table public.execution_worker_commands
+  drop constraint if exists execution_worker_commands_command_check;
+
+alter table public.execution_worker_commands
+  add constraint execution_worker_commands_command_check
+  check (command in ('FOCUS_SESSION','PRODUCT_CHECK','SUBMIT_OTP'));
