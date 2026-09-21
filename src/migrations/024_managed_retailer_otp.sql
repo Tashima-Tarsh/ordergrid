7e6deb2a05315247b0c24e4bbe997ebef43dac89
NITISH KUMAR <amyhod3@gmail.com>
2026-09-21 10:13:19 +0530

    Track managed retailer session challenge code

--- src/migrations/024_managed_retailer_otp.sql
+++ src/migrations/024_managed_retailer_otp.sql
@@ -5,3 +5,6 @@ alter table public.execution_worker_commands
 alter table public.execution_worker_commands
   add constraint execution_worker_commands_command_check
   check (command in ('FOCUS_SESSION','PRODUCT_CHECK','SUBMIT_OTP'));
+
+alter table public.retailer_accounts
+  add column if not exists session_challenge_code text;
